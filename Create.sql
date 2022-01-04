cb4c96a30df593d6042afa7dd02710d1a4029a2b
Arkadiusz Biały <arkadiusz.bialy@onet.pl>
2022-01-04 15:10:28 +0100

    Updated create.sql
    Added tables for TramModels, BusModels, SpecialVehicleModels, VehicleModels, Depots, PeriodicTickets, Passengers, Discounts, AirReadings, SoldSingleTickets and TypesOfTickets

--- Create.sql
+++ Create.sql
@@ -10,6 +10,17 @@ IF OBJECT_ID('TramDrivers', 'U') IS NOT NULL DROP TABLE TramDrivers
 IF OBJECT_ID('ServiceTechnicians', 'U') IS NOT NULL DROP TABLE ServiceTechnicians
 IF OBJECT_ID('TicketInspectors', 'U') IS NOT NULL DROP TABLE TicketInspectors
 IF OBJECT_ID('OfficeWorkers', 'U') IS NOT NULL DROP TABLE OfficeWorkers
+IF OBJECT_ID('TramModels', 'U') IS NOT NULL DROP TABLE TramModels
+IF OBJECT_ID('BusModels', 'U') IS NOT NULL DROP TABLE BusModels
+IF OBJECT_ID('SpecialVehicleModels', 'U') IS NOT NULL DROP TABLE SpecialVehicleModels
+IF OBJECT_ID('VehicleModels', 'U') IS NOT NULL DROP TABLE VehicleModels
+IF OBJECT_ID('Depots', 'U') IS NOT NULL DROP TABLE Depots
+IF OBJECT_ID('PeriodicTickets', 'U') IS NOT NULL DROP TABLE PeriodicTickets
+IF OBJECT_ID('Passengers', 'U') IS NOT NULL DROP TABLE Passengers
+IF OBJECT_ID('Discounts', 'U') IS NOT NULL DROP TABLE Discounts
+IF OBJECT_ID('AirReadings', 'U') IS NOT NULL DROP TABLE AirReadings
+IF OBJECT_ID('SoldSingleTickets ', 'U') IS NOT NULL DROP TABLE SoldSingleTickets
+IF OBJECT_ID('TypesOfTickets', 'U') IS NOT NULL DROP TABLE TypesOfTickets
 
 
 CREATE TABLE Departments (
@@ -140,6 +151,125 @@ CREATE TABLE OfficeWorkers (
 	FOREIGN KEY (EmployeeID, DepartmentID) REFERENCES Employees (EmployeeID, DepartmentID)
 )
 
+CREATE TABLE VehicleModels (
+ModelID INT PRIMARY KEY,
+ProductionStartDate DATE,
+ProductionEndDate DATE,
+Seats INT NOT NULL,
+StandingPlaces INT NOT NULL,
+ModelLength INT NOT NULL --, 
+--FOREIGN KEY (ModelID) REFERENCES Vehicles(ModelID)
+--ON DELETE CASCADE
+--ON UPDATE CASCADE
+)
+
+CREATE TABLE TramModels (
+ModelID INT PRIMARY KEY,
+LowFloor BIT NOT NULL,
+NumberOfCarriages INT NOT NULL,
+Bidirectional BIT NOT NULL,
+
+FOREIGN KEY (ModelID) REFERENCES VehicleModels(ModelID)
+ON DELETE CASCADE
+ON UPDATE CASCADE
+)
+
+CREATE TABLE BusModels (
+ModelID INT PRIMARY KEY,
+Drive VARCHAR(50) NOT NULL,
+LowFloor BIT NOT NULL,
+Articulated BIT NOT NULL,
+
+FOREIGN KEY (ModelID) REFERENCES VehicleModels(ModelID)
+ON DELETE CASCADE
+ON UPDATE CASCADE
+)
+
+CREATE TABLE SpecialVehicleModels (
+ModelID INT PRIMARY KEY,
+Remarks VARCHAR(50),
+
+FOREIGN KEY (ModelID) REFERENCES VehicleModels(ModelID)
+ON DELETE CASCADE
+ON UPDATE CASCADE
+)
+
+CREATE TABLE Depots (
+DepotID INT PRIMARY KEY,
+BuildingID INT NOT NULL,
+TramDepot BIT NOT NULL,
+BusDepot BIT NOT NULL,
+
+FOREIGN KEY (BuildingID) REFERENCES Buildings(BuildingID)
+ON DELETE CASCADE
+ON UPDATE CASCADE
+)
+
+CREATE TABLE TypesOfTickets (
+TicketID INT PRIMARY KEY,
+Name VARCHAR(50) NOT NULL,
+Price MONEY NOT NULL,
+Periodic BIT NOT NULL
+)
+
+CREATE TABLE Passengers (
+PassengerID INT PRIMARY KEY,
+PESEL CHAR(11) UNIQUE NOT NULL,
+FirstName VARCHAR(50) NOT NULL,
+LastName VARCHAR(50) NOT NULL,
+BirthDate DATE NOT NULL,
+PlaceOfResidence VARCHAR(50) NOT NULL,
+Student BIT NOT NULL,
+Pupil BIT NOT NULL,
+HonoraryBloodDonor BIT NOT NULL,
+Pensioner BIT NOT NULL
+)
+
+CREATE TABLE PeriodicTickets (
+TicketID INT PRIMARY KEY,
+TicketNumber INT NOT NULL,
+OwnerID INT NOT NULL,
+DateFrom DATE NOT NULL,
+
+FOREIGN KEY (TicketID) REFERENCES TypesOfTickets(TicketID)
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+
+FOREIGN KEY (OwnerID) REFERENCES Passengers(PassengerID)
+ON DELETE CASCADE
+ON UPDATE CASCADE
+)
+
+CREATE TABLE Discounts (
+DiscountID INT PRIMARY KEY,
+TicketID INT NOT NULL,
+DateFrom DATE,
+DateTo DATE
+
+FOREIGN KEY (TicketID) REFERENCES TypesOfTickets(TicketID)
+ON DELETE CASCADE
+ON UPDATE CASCADE
+)
+
+CREATE TABLE AirReadings (
+AirReadingDate Date PRIMARY KEY,
+PM10 FLOAT NOT NULL,
+PM25 FLOAT NOT NULL,
+SO2 FLOAT NOT NULL,
+NO2 FLOAT NOT NULL,
+O3 FLOAT NOT NULL
+)
+
+CREATE TABLE SoldSingleTickets (
+TicketID INT PRIMARY KEY,
+TicketNumber INT NOT NULL,
+DateOfPurchase Date NOT NULL
+
+FOREIGN KEY (TicketID) REFERENCES TypesOfTickets(TicketID)
+ON DELETE CASCADE
+ON UPDATE CASCADE
+)
+
 
 ALTER TABLE Employees
 ADD CONSTRAINT is_phone_valid CHECK(ISNUMERIC(PhoneNumber) = 1 AND LEN(PhoneNumber) = 9)
@@ -161,3 +291,12 @@ ADD CONSTRAINT is_tram_licence_valid CHECK(LEN(LicenceID) = 10)
 
 ALTER TABLE TicketInspectors
 ADD CONSTRAINT is_ispector_licence_valid CHECK(LEN(LicenceID) = 10)
+
+ALTER TABLE VehicleModels
+ADD CONSTRAINT is_production_date_valid CHECK(ProductionStartDate >= ProductionEndDate)
+
+ALTER TABLE Passengers
+ADD CONSTRAINT is_passenger_pesel_valid CHECK(ISNUMERIC(PESEL) = 1 AND LEN(PESEL) = 11)
+
+ALTER TABLE Discounts
+ADD CONSTRAINT is_discount_date_valid CHECK(DateFrom >= DateTo)
