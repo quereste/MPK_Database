65fdd8cb2d760b0df9e507cb1550f03e5611445b
Arkadiusz Biały <arkadiusz.bialy@onet.pl>
2022-02-11 13:40:31 +0100

    update views.sql

--- Views.sql
+++ Views.sql
@@ -37,3 +37,20 @@ AS
 	GROUP BY M.ModelName, M.ModelID
 	ORDER BY M.ModelID
 	OFFSET 0 ROWS
+	
+GO
+CREATE VIEW Passengers_With_Periodic_Tickets
+AS
+	SELECT * FROM Passengers P JOIN PeriodicTickets PT ON P.PassengerID = PT.OwnerID
+GO
+
+CREATE VIEW Single_Tickets_Sold_Each_Day
+AS
+	SELECT DateOfPurchase, COUNT(*) AS TicketsSold FROM SoldSingleTickets
+	GROUP BY DateOfPurchase
+GO
+
+CREATE VIEW Days_With_Discounts
+AS
+	SELECT AirReadingDate FROM AirReadings WHERE PM10 >= 150 OR PM25 >= 150 OR NO2 >= 150 OR SO2 >= 150 OR O3 >= 150
+GO
