d061a506dbdab6107b91f2e03af3aa18c7ba4aef
Arkadiusz Biały <arkadiusz.bialy@onet.pl>
2022-02-13 18:51:38 +0100

    Update Triggers.sql

--- Triggers.sql
+++ Triggers.sql
@@ -4,6 +4,12 @@ DROP TRIGGER New_Discounts
 IF OBJECT_ID('Air_Readings_Update', 'TR') IS NOT NULL
 DROP TRIGGER Air_Readings_Update
 
+IF OBJECT_ID('Passenegers_Data_Check_Insert', 'TR') IS NOT NULL
+DROP TRIGGER Passenegers_Data_Check_Insert
+
+IF OBJECT_ID('Passenegers_Data_Check_Update', 'TR') IS NOT NULL
+DROP TRIGGER Passenegers_Data_Check_Update
+
 GO
 CREATE TRIGGER InsertBuilding 
 ON Buildings 
@@ -102,4 +108,132 @@ AS
 
 	CLOSE cur
 	DEALLOCATE cur
+GO
+
+GO
+CREATE TRIGGER Passenegers_Data_Check_Insert ON Passengers
+INSTEAD OF INSERT
+AS
+	DECLARE @id INT
+	DECLARE @pesel CHAR(11)
+	DECLARE @firstName VARCHAR(50)
+	DECLARE @lastName VARCHAR(50)
+	DECLARE @address VARCHAR(50)
+	DECLARE @city VARCHAR(50)
+	DECLARE @birthDate DATE
+	DECLARE @student BIT
+	DECLARE @pupil BIT
+	DECLARE @honoraryBloodDonor BIT
+	DECLARE @pensioner BIT
+	DECLARE @years INT
+	DECLARE @gender VARCHAR(1)
+	DECLARE @errorMsg VARCHAR(50)
+
+	DECLARE cur CURSOR FOR SELECT PESEL, BirthDate, FirstName, LastName, PlaceOfResidence, City, Student, Pupil, HonoraryBloodDonor, Pensioner FROM inserted
+	OPEN cur
+	FETCH cur INTO @pesel, @birthDate, @firstName, @lastName, @address, @city, @student, @pupil, @honoraryBloodDonor, @pensioner
+	
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		SELECT @years = DATEDIFF(yy, @birthDate, GETDATE()) - CASE WHEN (MONTH(@birthDate) > MONTH(GETDATE())) OR (MONTH(@birthDate) = MONTH(GETDATE()) AND DAY(@birthDate) > DAY(GETDATE())) THEN 1 ELSE 0 END
+		SET @gender = (SELECT LEFT(RIGHT(CONVERT(VARCHAR, @pesel), 2), 1))
+		IF (@gender = '0' OR @gender = '2' OR @gender = '4' OR @gender = '6' OR @gender = '8')
+		BEGIN
+			IF (@years < 60 AND @pensioner = 1)
+			BEGIN
+				SET @errorMsg = @firstName + ' ' + @lastName + ' cannot be a pensioner.' 
+				;THROW 51001, @errorMsg, 1
+			END
+		END
+		ELSE
+		BEGIN
+			IF (@years < 65 AND @pensioner = 1)
+			BEGIN
+				SET @errorMsg = @firstName + ' ' + @lastName + ' cannot be a pensioner.' 
+				;THROW 51001, @errorMsg, 1
+			END
+		END
+		IF (@years > 26 AND @student = 1)
+		BEGIN
+			SET @errorMsg = @firstName + ' ' + @lastName + ' cannot be a student.' 
+			;THROW 51001, @errorMsg, 1
+		END
+		IF (@years > 18 AND @pupil = 1)
+		BEGIN
+			SET @errorMsg = @firstName + ' ' + @lastName + ' cannot be a pupil.' 
+			;THROW 51001, @errorMsg, 1
+		END
+		ELSE
+		BEGIN
+			INSERT INTO Passengers VALUES
+			(@pesel, @birthDate, @firstName, @lastName, @address, @city, @student, @pupil, @honoraryBloodDonor, @pensioner)
+		END
+
+		FETCH cur INTO @pesel, @birthDate, @firstName, @lastName, @address, @city, @student, @pupil, @honoraryBloodDonor, @pensioner
+	END
+
+	CLOSE cur
+	DEALLOCATE cur
+
+GO
+
+GO
+CREATE TRIGGER Passenegers_Data_Check_Update ON Passengers
+AFTER UPDATE
+AS
+	DECLARE @id INT
+	DECLARE @pesel CHAR(11)
+	DECLARE @firstName VARCHAR(50)
+	DECLARE @lastName VARCHAR(50)
+	DECLARE @address VARCHAR(50)
+	DECLARE @city VARCHAR(50)
+	DECLARE @birthDate DATE
+	DECLARE @student BIT
+	DECLARE @pupil BIT
+	DECLARE @honoraryBloodDonor BIT
+	DECLARE @pensioner BIT
+	DECLARE @years INT
+	DECLARE @gender VARCHAR(1)
+	DECLARE @errorMsg VARCHAR(50)
+
+	DECLARE cur CURSOR FOR SELECT PESEL, BirthDate, FirstName, LastName, PlaceOfResidence, City, Student, Pupil, HonoraryBloodDonor, Pensioner FROM inserted
+	OPEN cur
+	FETCH cur INTO @pesel, @birthDate, @firstName, @lastName, @address, @city, @student, @pupil, @honoraryBloodDonor, @pensioner
+	
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		SELECT @years = DATEDIFF(yy, @birthDate, GETDATE()) - CASE WHEN (MONTH(@birthDate) > MONTH(GETDATE())) OR (MONTH(@birthDate) = MONTH(GETDATE()) AND DAY(@birthDate) > DAY(GETDATE())) THEN 1 ELSE 0 END
+		SET @gender = (SELECT LEFT(RIGHT(CONVERT(VARCHAR, @pesel), 2), 1))
+		IF (@gender = '0' OR @gender = '2' OR @gender = '4' OR @gender = '6' OR @gender = '8')
+		BEGIN
+			IF (@years < 60 AND @pensioner = 1)
+			BEGIN
+				SET @errorMsg = @firstName + ' ' + @lastName + ' cannot be a pensioner.' 
+				;THROW 51002, @errorMsg, 1
+			END
+		END
+		ELSE
+		BEGIN
+			IF (@years < 65 AND @pensioner = 1)
+			BEGIN
+				SET @errorMsg = @firstName + ' ' + @lastName + ' cannot be a pensioner.' 
+				;THROW 51002, @errorMsg, 1
+			END
+		END
+		IF (@years > 26 AND @student = 1)
+		BEGIN
+			SET @errorMsg = @firstName + ' ' + @lastName + ' cannot be a student.' 
+			;THROW 51002, @errorMsg, 1
+		END
+		IF (@years > 18 AND @pupil = 1)
+		BEGIN
+			SET @errorMsg = @firstName + ' ' + @lastName + ' cannot be a pupil.' 
+			;THROW 51002, @errorMsg, 1
+		END
+		FETCH cur INTO @pesel, @birthDate, @firstName, @lastName, @address, @city, @student, @pupil, @honoraryBloodDonor, @pensioner
+	END
+
+	CLOSE cur
+	DEALLOCATE cur
+
 GO
