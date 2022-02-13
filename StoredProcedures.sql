62361966bec7ada153ce08daf28f7da0470983ec
Arkadiusz Biały <arkadiusz.bialy@onet.pl>
2022-02-13 19:14:12 +0100

    Update StoredProcedures.sql

--- StoredProcedures.sql
+++ StoredProcedures.sql
@@ -25,6 +25,9 @@ DROP PROCEDURE Add_Inspector
 IF OBJECT_ID('Add_Office_Worker', 'P') IS NOT NULL
 DROP PROCEDURE Add_Office_Worker
 
+IF OBJECT_ID('Delete_Passengers_With_Invalid_Periodic_Tickets', 'P') IS NOT NULL
+DROP PROCEDURE Delete_Passengers_With_Invalid_Periodic_Tickets
+
 GO
 
 CREATE PROCEDURE Passenger_Ticket_Info (@id INT) AS
@@ -100,6 +103,39 @@ BEGIN
 	SELECT * FROM @result
 END
 GO
+
+CREATE PROCEDURE Delete_Passengers_With_Invalid_Periodic_Tickets (@currentDay DATE) AS
+BEGIN
+	DECLARE cur CURSOR FOR SELECT TicketNumber, PT.TicketID, OwnerID, DateFrom, PassengerID, FirstName, LastName FROM PeriodicTickets PT JOIN TypesOfTickets T ON PT.TicketID = T.TicketID
+	JOIN Passengers P ON PT.OwnerID = P.PassengerID
+
+	OPEN cur
+	DECLARE @ticketID INT
+	DECLARE @ownerID INT
+	DECLARE @dateFrom DATE
+	DECLARE @days INT
+	DECLARE @isValid BIT
+	DECLARE @firstName VARCHAR(50)
+	DECLARE @lastName VARCHAR(50)
+	DECLARE @id INT
+	DECLARE @ticketNumber CHAR(15)
+	FETCH cur INTO @ticketNumber, @ticketID, @ownerID, @dateFrom, @id, @firstName, @lastName
+	DECLARE @daysDiff INT
+	
+	WHILE (@@FETCH_STATUS = 0)
+	BEGIN
+		IF (@dateFrom <= @currentDay)
+		BEGIN
+			SET @daysDiff = DATEDIFF(day, @dateFrom, @currentDay)
+			IF NOT ((@ticketID BETWEEN 18 AND 21 AND @daysDiff <= 30) OR (@ticketID BETWEEN 22 AND 23 AND @daysDiff <= 180))
+				DELETE FROM PeriodicTickets WHERE TicketNumber = @ticketNumber
+		END
+		FETCH cur INTO @ticketNumber, @ticketID, @ownerID, @dateFrom, @id, @firstName, @lastName
+	END
+
+	CLOSE cur
+	DEALLOCATE cur
+END
 
 GO
 -- view ticket prices on a day passed as an argument
